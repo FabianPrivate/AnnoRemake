df2b5ff84ef43db88d77fc8e86e9980de923b4f9
Fabian Mengerink <fabian_mengerink@hotmail.com>
2011-11-17 21:47:09 +0100

    reader

--- src/readers/BuildingReader.java
+++ src/readers/BuildingReader.java
@@ -1,12 +1,29 @@
 package readers;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
+import fileReaders.Sheet;
+
+import jxl.Workbook;
+
 public class BuildingReader {
 	protected String name;
 	
-	protected
+	protected InputStreamReader inputStreamReader;
+	protected BufferedReader input;
 	
 	public void read(String fileName) {
-		
+		File inputWorkbook = new File(fileName);
+        Workbook w;
+        try {	
+			  w = Workbook.getWorkbook(inputWorkbook);
+            Sheet sheet = w.getSheet(0); 
+        }
+        
 	}
 
 }
