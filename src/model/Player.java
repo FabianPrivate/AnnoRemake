778e5fbb3db3c9edfb0a05a99983e0e69495cde0
Fabian Mengerink <fabian_mengerink@hotmail.com>
2011-11-28 07:35:10 +0100

    Warehouse and lumberjack

--- src/model/Player.java
+++ src/model/Player.java
@@ -5,11 +5,21 @@ import java.util.HashMap;
 
 import javax.annotation.Resource;
 
+import model.buildings.Building;
+
 public class Player {
 	private HashMap<Resource, Integer> resources = new HashMap<Resource, Integer>();
 	private ArrayList<Building> buildings = new ArrayList<Building>();
+	
+	public Player() {
+		
+	}
+	
 	public void update() {
 		
+	}
+	
+	public void addBuilding(Building building) {
 		
 	}
 	
